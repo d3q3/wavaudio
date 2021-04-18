90ee8ef28ab0077777c9cc91569681f9b5b3b98d
d3q3 <projectd3q@drikus.net>
2021-04-19 00:00:59 +0200

    Form1.cs with new data providers

--- winmmAudio/testWavOut/Form1.cs
+++ winmmAudio/testWavOut/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
-using WavFiles;
+//using WavFiles;
+using D3Q.WavFile;
 using WavOutLib;
 
 namespace testWavOut
@@ -8,15 +9,14 @@ namespace testWavOut
     public partial class Form1 : Form
     {
         WavOutPlayer waveOut;
-        LoopedDataProvider dataProvider;
-        //FixedLengthDataProvider dataProvider;
+        
+        FixedLengthDataProvider dataProvider; //another option: LoopedDataProvider dataProvider;
         const int HeaderCount = 6;
 
         public Form1()
         {
             InitializeComponent();
         }
-
 
         private void btnInit_Click(object sender, EventArgs e)
         {
@@ -28,27 +28,20 @@ namespace testWavOut
 
         private void btnOpen_Click(object sender, EventArgs e)
         {
-            // D3Q: read data from wav-file; 038-d and 052-e are organ-pipes; toccata is, well... toccata.
-            WavFile wavFile = new WavFile();
-            // "../../sound/038-d.wav", 16 bit, used with a LoopDataProvider
-            // "../../sound/052-e.wav", 24 bit, used with a LoopDataProvider
-            //wavFile.ReadWavFileByteArray("../../sound/038-d.wav");
-            //"../../sound/Toccata-and-fugue-in-d-minor.wav" used with a FixedLengthDataProvider
-            wavFile.ReadWavFileByteArray("../../sound/Toccata-and-fugue-Short.wav");
+            // D3Q: read data from wav-files; 038-d and 052-e are organ-pipes; toccata is, well... toccata.
+            // data read from Wav-file using class Wav in Wav-project
+            Wav WavFile = new Wav("../../sound/Toccata-and-fugue-Short.wav");
 
-            FrameReader frameReader = wavFile.getFrameReader();
-            frameReader.startFrame(0);
+            FrameReader frameReader = WavFile.getFrameReader();
+            frameReader.Start(0);
 
-            // D3Q: create a dataprovider; a loopedDataprovider repeats itself in a loop
-            //dataProvider = new LoopedDataProvider(frameReader.wavBytes, frameReader.currentByte, frameReader.findFrameAfterTime(1.20747916666667f),
-            //    frameReader.findFrameAfterTime(2.7106875f), 6000, frameReader.bytesInValue());
-            //dataProvider = new FixedLengthDataProvider(frameReader.wavBytes, frameReader.currentByte, 2200, frameReader.bytesInValue());
-            dataProvider = new LoopedDataProvider(frameReader.wavBytes, frameReader.currentByte, frameReader.findFrameAfterTime(0.0f),
-                frameReader.findFrameAfterTime(120.0f), 2200, frameReader.bytesInValue());
+            //loopedProvider = new LoopedDataProvider(frameReader, 200000, frameReader.FrameCount, 2500);
+            dataProvider = new FixedLengthDataProvider(frameReader, 2500);
 
             // D3Q: open device
-            WaveFormatEx waveFormatEx = waveOut.createWaveOutFormat(frameReader.sampleRate, frameReader.bitsSample, frameReader.channelCount);
-            // if deviceId==-1 the default output is used 
+            FmtSubChunk fmt = WavFile.FmtChunk;
+            WaveFormatEx waveFormatEx = waveOut.createWaveOutFormat(fmt.SampleRate, fmt.BitsPerSample, fmt.NumChannels);
+            // if deviceId == -1 the default output is used 
             int deviceId = listBox1.SelectedIndex;
             int res = waveOut.openDevice(waveFormatEx, deviceId);
             if (res != 0) { Console.WriteLine(waveOut.getWavOutError(res)); Console.ReadLine(); }
@@ -80,9 +73,7 @@ namespace testWavOut
         {
             waveOut.closeDevice();
         }
-
     }
-
 
 
     /// <summary>
@@ -90,93 +81,70 @@ namespace testWavOut
     /// </summary>
     class FixedLengthDataProvider : BufferDataProvider
     {
-        byte[] buffer;
-        // D3Q: constructor translates frame-count to byte-count
-        int currentByte, countBytes;
-        // D3Q: index first byte audio in buffer
-        int nulByte;
-        // D3Q: bytes in each frame, e.g. when 24bits, 2 channels then bytesInFrame = 6.
-        int bytesInFrame;
+        FrameReader frameReader;
+        int frameNextCount;
 
-        /// <summary>
-        /// D3Q: initializes the data provider. 
-        /// </summary>
-        /// <param name="buffer"></param>
-        /// <param name="nulByte">index of first audio byte in buffer</param>
-        /// <param name="frameCount">count in frames of each buffer when calling getBuffer()</param>
-        /// <param name="bytesInFrame">e.g. for 24bits and 2 channels: 6 bytes</param>
-        public FixedLengthDataProvider(byte[] buffer, int nulByte, int frameCount, int bytesInFrame)
+        public FixedLengthDataProvider(FrameReader frameReader, int frameNextCount)
         {
-            this.buffer = buffer; this.currentByte = nulByte; this.nulByte = nulByte; this.countBytes = frameCount*bytesInFrame;
-            this.bytesInFrame = bytesInFrame;
+            this.frameReader = frameReader;
+            this.frameNextCount = frameNextCount;
 
             this.getNextBuffer = getBuffer;
         }
 
         public override void reset(int atFrame)
         {
-            this.currentByte = nulByte+atFrame* bytesInFrame;
+            frameReader.Start(atFrame);
         }
 
         private bool getBuffer(out byte[] buffer, out int start, out int count)
         {
             bool more = true;
-            buffer = this.buffer;
-            start = this.currentByte;
-            int headerStart = this.currentByte;
-            if (buffer.Length >= currentByte + countBytes) currentByte += countBytes; else { currentByte = buffer.Length; more = false; }
-            count = currentByte - headerStart;
+            int toCopy = frameNextCount;
+            if (frameReader.CurrentFrame + frameNextCount > frameReader.FrameCount)
+            {
+                toCopy = frameReader.FrameCount - frameReader.CurrentFrame;
+                more = false;
+            }
+            buffer = frameReader.ReadNextFrames(toCopy, out int framesCopied, out int firstByte, out int byteCount);
+            start = firstByte;
+            count = byteCount;
             return more;
         }
     }
 
     /// <summary>
-    /// Provides the data used to fill the buffers of the output device.
+    /// D3Q: Provides the data used to fill the buffers of the output device. The data loops from loopStartTime to loopEndTime.
     /// </summary>
     class LoopedDataProvider : BufferDataProvider
     {
-        byte[] buffer;
-        // D3Q: constructor translates frame-count to byte-count
-        int currentByte, loopStartByte, loopEndByte, countBytes;
-        // D3Q: index first byte audio in buffer
-        int nulByte;
-        // D3Q: bytes in each frame, e.g. when 24bits, 2 channels then bytesInFrame = 6.
-        int bytesInFrame;
-
-        /// <summary>
-        /// D3Q: initializes the data provider. 
-        /// </summary>
-        /// <param name="buffer"></param>
-        /// <param name="nulByte">index of first audio byte in buffer</param>
-        /// <param name="loopStartFrame">start of loop using frame-count</param>
-        /// <param name="loopEndFrame">end of loop using frame-count</param>
-        /// <param name="frameCount">optimal count in frames of each buffer when calling getBuffer()</param>
-        /// <param name="bytesInFrame">e.g. for 24bits and 2 channels: 6 bytes</param>
-        public LoopedDataProvider(byte[] buffer, int nulByte, int loopStartFrame, int loopEndFrame, int frameCount, int bytesInFrame)
+        int loopStartFrame, loopEndFrame, frameNextCount;
+        FrameReader frameReader;
+        public LoopedDataProvider(FrameReader frameReader, int loopStartFrame, int loopEndFrame, int frameNextCount)
         {
-            this.buffer = buffer; this.currentByte = nulByte; this.nulByte = nulByte;
-            this.countBytes = frameCount*bytesInFrame;
-            this.loopStartByte = nulByte+loopStartFrame*bytesInFrame; this.loopEndByte = nulByte+loopEndFrame*bytesInFrame;
-            this.bytesInFrame = bytesInFrame;
+            this.frameReader = frameReader;
+            this.loopStartFrame = loopStartFrame;
+            this.loopEndFrame = loopEndFrame-1; // endframe is included, so -1
+            this.frameNextCount = frameNextCount;
 
             this.getNextBuffer = getBuffer;
         }
 
-        public override void reset(int atFrame)
-        {
-            this.currentByte = nulByte + atFrame * bytesInFrame;
+        public override void reset(int atFrame) { 
+            frameReader.Start(atFrame);
         }
 
         private bool getBuffer(out byte[] buffer, out int start, out int count)
         {
             bool more = true;
-            buffer = this.buffer;
-            start = this.currentByte;
-            int headerStart = this.currentByte;
-            if (loopEndByte >= currentByte + countBytes) currentByte += countBytes; else { currentByte = loopEndByte; }
-            count = currentByte - headerStart;
-            if (currentByte == loopEndByte) currentByte = loopStartByte;
-            //Console.WriteLine(">> " + loopStartByte + " > " + loopEndByte+ " > " + start + " > " + count);
+            int toCopy = frameNextCount;
+            if (frameReader.CurrentFrame + frameNextCount > frameReader.FrameCount) toCopy = frameReader.FrameCount - frameReader.CurrentFrame;
+            buffer = frameReader.ReadNextFrames(toCopy, out int framesCopied, out int firstByte, out int byteCount);
+            start = firstByte;
+            count = byteCount;
+
+            if ( frameReader.CurrentFrame>=loopEndFrame)
+                frameReader.Start(loopStartFrame);
             return more;
         }
     }
